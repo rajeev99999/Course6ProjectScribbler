e1032379c9efa3a4ade7b499daec0b924925a0f1
rajeev99999 <rajeevrevada@gmail.com>
2021-01-19 22:44:58 +0530

    Final Commit

--- post.js
+++ post.js
@@ -1,5 +1,9 @@
 var userName,postHeading,postContent;
-
+userName = localStorage.getItem("userName");
+postHeading = localStorage.getItem("postHeading");
+postContent = localStorage.getItem("postContent");
+var countLikes = 0;
+var commentsArray = [];
 
 // Get the modal
 
@@ -59,3 +63,73 @@ $(document).ready(function(){
   postContents.innerText = postContent;
 });
 
+function editDetails(){
+
+  var btnedit =  document.getElementById("btn");
+  var btnSave = document.getElementById("btnSave");
+  btnSave.hidden = false;
+  btnedit.hidden = true;
+  let posttitle = document.getElementById("post-heading");
+  let postcontent = document.getElementById("post-content");
+  posttitle.contentEditable = true;
+  postcontent.contentEditable = true;
+
+
+}
+
+function saveDetails(){
+  let btnedit =  document.getElementById("btn");
+  let btnSave = document.getElementById("btnSave");
+  let postcontentUpd = document.getElementById("post-content");
+  var postTitleUpd = document.getElementById("post-heading");
+  postTitleUpd.contentEditable = false;
+  postcontentUpd.contentEditable = false;
+  let editedPostheading = document.getElementById("post-heading");
+  btnSave.hidden = true;
+  btnedit.hidden = false;
+  if(postHeading!==postTitleUpd.innerText || postContent!==postcontentUpd.innerText){
+  
+    var updatedPostheading = editedPostheading.innerText;
+    if (updatedPostheading.indexOf("UPDATED: ") === -1){
+    editedPostheading.innerText = "UPDATED: " + editedPostheading.innerText;
+    localStorage.setItem("postHeading",editedPostheading.innerText);
+    }
+
+  }
+}
+
+function likePost(){
+  let likebtn  = document.getElementsByClassName("like-btn")[0];
+  let likedbtn = document.getElementsByClassName("like-btn")[1];
+  let likes = document.getElementById("likes");
+  let likesMore = document.getElementById("likesMore");
+  let noOfLikes = document.getElementById("noOfLikes");
+  countLikes+=1;
+  if(countLikes>1){
+    likes.hidden = true;
+    likesMore.hidden = false;
+    noOfLikes.innerText = countLikes;
+  }
+  else{
+  likes.hidden = false;
+  likesMore.hidden = true;
+  }
+  likebtn.hidden=true;
+  likedbtn.hidden=false;
+  let subsOfPost = document.getElementById("subs-like-text");
+  subsOfPost.style.display ="none";
+}
+
+function comment(){
+  let commentsId = document.getElementById("box"); 
+  let commentstext = commentsId.value;
+  let commentsContainer = document.getElementById("comment-box");
+  commentsArray.push(commentstext);
+  let commentsLen = commentsArray.length;
+  var x ="", i;
+for (i=0; i<commentsLen; i++) {
+  x = x + "<p class='borderBox'> " + commentsArray[i] + "</p>";
+}
+document.getElementById("comment-box").innerHTML = x;
+commentsId.value = "";
+}
